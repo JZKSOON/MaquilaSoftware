415625a77a4148473f670bf27e165cbb7a091de1
JZKSOON <joscasatilla64@gmail.com>
2025-04-09 22:28:12 -0600

    Conexion de la clase main con la vista LoginView.fxml

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,6 +1,9 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -11,6 +14,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        // Cargar el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/LoginView.fxml"));
+        Parent root = loader.load();
+
+        // Configurar la escena y el escenario
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Login");
         stage.show();
     }
 }
