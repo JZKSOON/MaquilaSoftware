ca0899836b7e62ddfcfeb2a44c16b46819996eea
JZKSOON <joscastilla64@gmail.com>
2025-04-09 21:05:38 -0600

    Primer commit del proyecto MaquilaSoftware

---
+++ src/main/java/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
