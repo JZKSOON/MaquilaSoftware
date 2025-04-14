15c9ec283fc343cc8008bee4614d9fcceafb0bd3
Marcrvrag <riveradelangelmarcoantonio@gmail.com>
2025-04-13 21:23:23 -0600

    -Agregar dos botones al menu de MainViewClient.fxml que hacen un sout

--- src/main/java/controllers/MainViewClientController.java
+++ src/main/java/controllers/MainViewClientController.java
@@ -1,4 +1,25 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainViewClientController {
+
+    @FXML
+    private Button btnCliente;
+
+    @FXML
+    private Button btnInicio;
+
+    @FXML
+    void btnCliente(ActionEvent event) {
+        System.out.println("Cliente");
+    }
+
+    @FXML
+    void btnInicio(ActionEvent event) {
+        System.out.println("Inicio");
+    }
+
 }
