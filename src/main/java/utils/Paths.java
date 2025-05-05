2876c3bfdaea4ede37f48a59673e61892ed77cf1
JZKSOON <joscasatilla64@gmail.com>
2025-05-05 02:02:14 -0600

    ACTUALIZACION GENERAL 3

--- src/main/java/utils/Paths.java
+++ src/main/java/utils/Paths.java
@@ -5,4 +5,5 @@ public class Paths {
     public static final String MainViewAdm = "/Views/Interfaz/MainViewAdm.fxml";
     public static final String LoginView = "/Views/Interfaz/LoginView.fxml";
     public static final String MainMaqView = "/Views/Interfaz/MainMaqView.fxml";
+    public static final String CortesView = "/Views/ViewsAdm/CortesView.fxml";
 }
